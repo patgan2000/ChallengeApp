1a26dd9b9391002a70b183b4c74788852c19825c
Patrycja Gancarczyk <patgan2000@gmail.com>
2023-04-27 19:52:15 +0200

    Zad 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name_1 = "Ewa";
+var age_1 = 12;
+string sex_1 = "Kobieta";
+
+if (age_1 < 30 && sex_1 == "Kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (name_1 == "Ewa" && age_1 == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
+else if (age_1 < 18 && sex_1 != "Kobieta")
+{
+    Console.WriteLine("Niepełnoletni mężczyznaa");
+}
