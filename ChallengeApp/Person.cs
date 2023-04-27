279bf74d075896bc731a608002038ce5903461e2
Patrycja Gancarczyk <patgan2000@gmail.com>
2023-04-27 22:41:23 +0200

    Ex. 14

--- ChallengeApp/Person.cs
+++ ChallengeApp/Person.cs
@@ -7,22 +7,18 @@ public abstract class Person
     public Person()
         : this ("no name")
     {
-        
     }
-
     public Person(string name)
         : this (name, "NO surname")
     {
         this.name = "no name";
     }
-
     public Person(string name, string surname)
         : this (name, surname, 'M')
     {
         this.name = name;
         this.surname = surname;
     }
-
     public Person(string name, string surname, char sex)
         : this (name, surname, sex, 20)
     {
@@ -30,7 +26,6 @@ public abstract class Person
         this.surname= surname;
         this.sex = sex;
     }
-
     public Person(string name, string surname, char sex, int age)
 
     {
@@ -40,13 +35,9 @@ public abstract class Person
         this.age= age;
     }
 
-
     public string name { get; private set; }
-
     public string surname { get; private set; }
-    
     public char sex { get; private set; }
-
     public int age { get; private set; }
 
 
