b753684047647cc6cc0f12ccc1ef8e2be0402365
Patrycja Gancarczyk <patgan2000@gmail.com>
2023-04-27 20:03:36 +0200

    Ex. 8

---
+++ ChallengeAppTests/TypeTests.cs
@@ -0,0 +1,34 @@
+﻿namespace ChallengeAppTests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void IntAsValuableType()
+        {
+            int score1 = 10;
+            int score2 = 10;
+
+            Assert.AreEqual(score1, score2);
+        }
+        [Test]
+        public void StringAsValuableType()
+        {
+            string name1 = "Anna";
+            string name2 = "Anna";
+
+            Assert.AreEqual(name1, name2);
+        }
+        [Test]
+        public void VarAsReferenceType()
+        {
+            var em1 = GetEmployee("Anna");
+            var em2 = GetEmployee("Anna");
+
+            Assert.AreNotEqual(em1, em2);
+        }
+        private Employee GetEmployee(string name)
+        {
+            return new Employee(name);
+        }
+    }
+}
