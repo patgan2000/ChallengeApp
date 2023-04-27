df095be20e3ef92e4f4177545c8655fe7918f6da
Patrycja Gancarczyk <patgan2000@gmail.com>
2023-04-27 19:53:22 +0200

    Zad 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,18 +1,68 @@
-﻿string name_1 = "Ewa";
-var age_1 = 12;
-string sex_1 = "Kobieta";
+﻿int No = 2759071;
+string NoInString = No.ToString();
+char[] letters = NoInString.ToCharArray();
+Console.WriteLine("To twoja liczba: " + No);
+int number0 = 0;
+int number1 = 0;
+int number2 = 0;
+int number3 = 0;
+int number4 = 0;
+int number5 = 0;
+int number6 = 0;
+int number7 = 0;
+int number8 = 0;
+int number9 = 0;
 
-if (age_1 < 30 && sex_1 == "Kobieta")
+foreach (char x in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (x == '0')
+    {
+        number0++;
+    }
+    else if (x == '1')
+    {
+        number1++;
+    }
+    else if (x == '2')
+    {
+        number2++;
+    }
+    else if (x == '3')
+    {
+        number3++;
+    }
+    else if (x == '4')
+    {
+        number4++;
+    }
+    else if (x == '5')
+    {
+        number5++;
+    }
+    else if (x == '6')
+    {
+        number6++;
+    }
+    else if (x == '7')
+    {
+        number7++;
+    }
+    else if (x == '8')
+    {
+        number8++;
+    }
+    else if (x == '9')
+    {
+        number9++;
+    }
 }
-
-else if (name_1 == "Ewa" && age_1 == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-
-else if (age_1 < 18 && sex_1 != "Kobieta")
-{
-    Console.WriteLine("Niepełnoletni mężczyznaa");
-}
+Console.WriteLine("0 => " + number0);
+Console.WriteLine("1 => " + number1);
+Console.WriteLine("2 => " + number2);
+Console.WriteLine("3 => " + number3);
+Console.WriteLine("4 => " + number4);
+Console.WriteLine("5 => " + number5);
+Console.WriteLine("6 => " + number6);
+Console.WriteLine("7 => " + number7);
+Console.WriteLine("8 => " + number8);
+Console.WriteLine("9 => " + number9);
